dc3274b376a62c6d1c0274edd04806ae79b73336
Michael Hermann <michael@srcinnovations.com.au>
2020-07-16 16:51:11 +1000

    bit of formatting

--- src/components/ThreadDetails.svelte
+++ src/components/ThreadDetails.svelte
@@ -3,13 +3,25 @@
 </script>
 
 <style>
-
+  .head {
+    font-weight: 800;
+    border-bottom: 1px solid rgb(0, 0, 0, 0.2);
+  }
+  .stack {
+    padding: 2px;
+    white-space: pre-line;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    word-break: break-word;
+  }
 </style>
 
 {#if thread}
   <div class="wrapper">
-    <div class="stack" style="">{thread.stack.join('')}</div>
-    <div class="stack" style="">{thread.nativeStack.join('')}</div>
+    <div class="head">Java Stack</div>
+    <div class="stack">{thread.stack.join('\n')}</div>
+    <div class="head">Native Stack</div>
+    <div class="stack">{thread.nativeStack.join('\n')}</div>
   </div>
 {:else}
   <div class="wrapper" />
